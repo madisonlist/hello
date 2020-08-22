53699868360986cab0eb6c4e6d514acb6ce4a7e0
Madison <mlist3@huskers.unl.edu>
2020-08-22 20:04:31 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: MAdison List
+ * Date: 2020
+ *
+ * A simple hello world program
+ **/
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int arg, char **argv) {
+
+    printf("Hello world\n");
+
+    return 0;
+
+}
